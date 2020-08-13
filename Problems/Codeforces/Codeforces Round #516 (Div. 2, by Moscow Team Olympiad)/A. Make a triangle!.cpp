b6a48c1907cb66759b77dc99109cfa66169141de
MaGnsi0 <magnsi0o0@gmail.com>
2020-08-13 23:31:14 +0200

    file changes

--- Problems/Codeforces/Codeforces Round #516 (Div. 2, by Moscow Team Olympiad)/A. Make a triangle!.cpp
+++ Problems/Codeforces/Codeforces Round #516 (Div. 2, by Moscow Team Olympiad)/A. Make a triangle!.cpp
@@ -3,11 +3,10 @@
 
 int main(){
 
-    std::vector<long> v(3); scanf("%ld %ld %ld", &v[0], &v[1], &v[2]);
+    std::vector<long> v(3);
+    scanf("%ld %ld %ld", &v[0], &v[1], &v[2]);
     sort(v.begin(), v.end());
     long diff = (1 + v[2] - v[1] - v[0]);
     diff = (diff < 0) ? 0 : diff;
     printf("%ld\n", diff);
-
-    return 0;
 }
