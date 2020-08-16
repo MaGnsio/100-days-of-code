bac228b3f097d77792a7bd92d5cc34bfb6ca9074
MaGnsi0 <magnsi0o0@gmail.com>
2020-08-17 00:29:56 +0200

    edit problems

--- Problems/Codeforces/Codeforces Round #664 (Div. 2)/B. Boboniu Plays Chess.cpp
+++ Problems/Codeforces/Codeforces Round #664 (Div. 2)/B. Boboniu Plays Chess.cpp
@@ -15,7 +15,7 @@ void solve (int i, int j)
 int main ()
 {
 	ios_base::sync_with_stdio (0); cin.tie (0); cout.tie (0);	
-	cin >> m >> n >> x >> y;
+	cin >> n >> m >> x >> y;
 	for (int i = 1; i <= n; ++i)
 	{
 		if (i & 1)
