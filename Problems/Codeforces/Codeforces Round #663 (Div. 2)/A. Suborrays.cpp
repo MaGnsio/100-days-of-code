03c41fe5c4f0181ce8628e8658c1600b6f990469
MaGnsi0 <magnsi0o0@gmail.com>
2020-08-16 21:18:40 +0200

    Day 9,10,11:solving problems

--- Problems/Codeforces/Codeforces Round #663 (Div. 2)/A. Suborrays.cpp
+++ Problems/Codeforces/Codeforces Round #663 (Div. 2)/A. Suborrays.cpp
@@ -21,3 +21,9 @@ int main ()
         solve () ;
     }
 }
+//Every permutation is good.
+/*Proof: We use the fact that for any set of numbers, it's bitwise OR is at least the maximum value in it.
+Now, we just need to show that any subarray of length len has at least one element greater than or equal to len.
+If the maximum element is <len, then, we have len elements all with values in the range [1,len−1].
+By the pigeonhole principle, at least 2 of them must be the same contradicting the fact the it's a permutation.*/
+//Time Complexity: O(n)
